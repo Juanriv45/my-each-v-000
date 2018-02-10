bdcf89eb96d33ccb08ee7e936cc1c5f985f2d965
Juan Rivera <juanriv45@gmail.com>
2018-02-10 22:42:52 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -5,22 +5,7 @@ def my_each(array) # put argument(s) here
   while i < array.length
     yield(array[i])
     i = i + 1
-end
-
-
-def hello_t(array)
-  i = 0
-
-  while i < array.length
-    yield(array[i])
-    i = i + 1
   end
-
   array
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
